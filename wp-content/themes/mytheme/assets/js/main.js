5ff3fdeab333b753945a74e3ccf2eb400750839a
Quang Dung <dphanquang@gmail.com>
2023-10-26 18:11:40 +0700

    'updatecode'

--- wp-content/themes/mytheme/assets/js/main.js
+++ wp-content/themes/mytheme/assets/js/main.js
@@ -2,53 +2,48 @@ $(document).ready(function () {
 	// let data = @json($events);
 	$.ajax({
 		url: "http://meo.test/wp-content/themes/mytheme/getdataservice.php",
-		data : {cmd : "get_all"},
-		type : 'POST'
+		data: { cmd: "get_all" },
+		type: 'POST',
+		dataType: 'json',
 	}).done(function (data) {
-		JSON.parse(data.result);
+		// JSON.parse(data);
 		console.log(data);
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev, next today',
+				center: 'title',
+				right: 'month, agendaWeek, agendaDay'
+			},
+			buttonText: {
+				prevYear: '&laquo;', // <<
+				nextYear: '&raquo;', // >>
+				today: '今日',
+				month: '月',
+				week: '週',
+				day: '日'
+			},
+
+			monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+
+			monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月',
+				'12月'
+			],
+
+			dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+
+			dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+
+			selectable: true,
+
+			selectHelper: true,
+			events: data
+		});
 	}).fail(function (jqxhr) {
 		console.log(jqxhr);
 	});
-	let data = [
-		{
-			"title": "test wordpress",
-			"start": "2023-10-20",
-			"start": "2023-10-20"
-		}
-	];
-	console.log(data);
 
-	$('#calendar').fullCalendar({
-		header: {
-			left: 'prev, next today',
-			center: 'title',
-			right: 'month, agendaWeek, agendaDay'
-		},
-		buttonText: {
-			prevYear: '&laquo;', // <<
-			nextYear: '&raquo;', // >>
-			today: '今日',
-			month: '月',
-			week: '週',
-			day: '日'
-		},
 
-		monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
 
-		monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月',
-			'12月'
-		],
-
-		dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
-
-		dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
-
-		selectable: true,
-
-		selectHelper: true,
-		// events: data
-	});
 	// calendar.render();
 
 	function renderOptionYearAnhMonth(yearStart) {
@@ -69,20 +64,16 @@ $(document).ready(function () {
 
 	function getDaysInMonth(month, year) {
 		let date = new Date(year, month, 1);
-
 		let days = [];
-		console.log(date.getMonth());
-		console.log(parseInt(month));
 		while (date.getMonth() === month) {
 			days.push(new Date(date).toLocaleDateString('sv'));
 			date.setDate(date.getDate() + 1);
 		}
-		console.log(days);
 		return days.reverse();
 	}
 
 
-	function renderDateTable(month, year) {
+	function renderDateTable(month, year, data) {
 		let listDay = getDaysInMonth(parseInt(month), year);
 		let html;
 		listDay.forEach(element => {
@@ -101,15 +92,14 @@ $(document).ready(function () {
 		console.log(yearAndMonth);
 		renderDateTable(yearAndMonth[1], yearAndMonth[0]);
 		$.ajax({
-			headers: {
-				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-			},
 			type: 'POST',
-			url: '/query',
+			url: 'http://meo.test/wp-content/themes/mytheme/getdataservice.php',
 			data: {
-				monthAndYear: data
+				monthAndYear: data,
+				cmd: 'getDataByMonthAndYear'
 			}
 		}).done(function (data) {
+			renderDateTable(yearAndMonth[1], yearAndMonth[0], data);
 			console.log(data);
 		}).fail(function (jqxhr) {
 			console.log(jqxhr);
--- wp-content/themes/mytheme/getdataservice.php
+++ wp-content/themes/mytheme/getdataservice.php
@@ -2,9 +2,13 @@
 include("database.php");
 
 
-var_dump($_POST["cmd"]);
+
 if (isset($_POST["cmd"]) && $_POST["cmd"] == "get_all") {
     getAllData();
+}
+
+if (isset($_POST["cmd"]) && $_POST["cmd"] == "getDataByMonthAndYear") {
+    getDataByMonthAndYear();
 }
 
 function getAllData()
@@ -22,14 +26,55 @@ function getAllData()
     while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
         $list[] = $row;
     }
+    $result = [];
+    foreach ($list as $key => $value) {
+        $title = $value['Keyword'] . ' ' . $value['Ranking'];
+        $result[] = [
+            "title" => $title,
+            "start" => $value['AddTime'],
+            "end" => $value['UpdateTime'],
+            "ranking" => $value['Ranking']
+        ];
+    }
 
-    // var_dump($query);
-    // exit;
-
+    header('Content-Type: application/json');
     echo json_encode(
-        $list
-
+        $result
     );
+}
+
+function getDataByMonthAndYear()
+{
+    include('database.php');
+    $monthAndYear = $_POST['monthAndYear'];
+    $arrayMonthAndYear = explode('-', $monthAndYear);
+
+    $list = [];
+    $query = "  SELECT T01_Ranking.KeywordNo, T01_Ranking.Ranking, M02_Keyword.Keyword, DATE(T01_Ranking.UpdateTime) AS UpdateTime, DATE(T01_Ranking.AddTime) AS AddTime
+                FROM T01_Ranking 
+                JOIN M02_Keyword 
+                ON T01_Ranking.KeywordNo = M02_Keyword.KeywordNo
+                WHERE MONTH(T01_Ranking.UpdateTime) = " . $arrayMonthAndYear[1] . " AND YEAR(T01_Ranking.UpdateTime) = " . $arrayMonthAndYear[0] . " ORDER BY T01_Ranking.Ranking ASC";
+
+    $sth = $pdo->query($query);
+    while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+        $list[] = $row;
+    }
+
+    $result = [];
+    foreach ($list as $key => $value) {
+        $title = $value['Keyword'] . ' ' . $value['Ranking'];
+        $result[] = [
+            "title" => $title,
+            "start" => $value['AddTime'],
+            "end" => $value['UpdateTime'],
+            "ranking" => $value['Ranking']
+        ];
+    }
+    // var_dump($monthAndYear);
+    // var_dump($list);
+    echo json_encode($result);
+
 }
 
 
